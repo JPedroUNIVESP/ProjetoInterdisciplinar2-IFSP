1ecf258720c37baececaf9d11add6235d27c9a58
jaquelinejana <jjds13@hotmail.com>
2023-12-06 19:47:37 -0300

    Adição de consultas

--- Queries-SQL/queries.sql
+++ Queries-SQL/queries.sql
@@ -100,3 +100,81 @@ FROM
 GROUP BY
     negotiation_type, furnished
 ORDER BY negotiation_type;
+
+-- Verificação de imóveis com o mesmo preço, porém um para locação e outro para venda.
+--(Esses casos podem ser sujeira na base)
+SELECT
+    a.price AS aluguel_price,
+    a.size AS aluguel_size,
+    a.rooms AS aluguel_rooms,
+    a.toilets AS aluguel_toilets,
+    a.suites AS aluguel_suites,
+    a.parking AS aluguel_parking,
+    a.district_zone AS aluguel_district_zone,
+    a.property_type AS aluguel_property_type,
+    v.price AS venda_price,
+    v.size AS venda_size,
+    v.rooms AS venda_rooms,
+    v.toilets AS venda_toilets,
+    v.suites AS venda_suites,
+    v.parking AS venda_parking,
+    v.district_zone AS venda_district_zone,
+    v.property_type AS venda_property_type
+FROM
+    aluguel a
+INNER JOIN
+    venda v ON a.price = v.price;
+
+-- Análise estatística da distribuição dos preços de venda na base de dados, agrupando as propriedades em diferentes
+-- faixas de preços e contabilizando os valores.
+SELECT
+    price_range,
+    COUNT(*) AS count_properties
+FROM (SELECT
+        CASE
+            WHEN price BETWEEN 0 AND 500000 THEN '0-500K'
+            WHEN price BETWEEN 500001 AND 1000000 THEN '500K-1M'
+            WHEN price BETWEEN 1000001 AND 1500000 THEN '1M-1.5M'
+            WHEN price BETWEEN 1500001 AND 2000000 THEN '1.5M-2M'
+            ELSE '2M ou mais'
+        END AS price_range,
+        CASE
+            WHEN price BETWEEN 0 AND 500000 THEN 1
+            WHEN price BETWEEN 500001 AND 1000000 THEN 2
+            WHEN price BETWEEN 1000001 AND 1500000 THEN 3
+            WHEN price BETWEEN 1500001 AND 2000000 THEN 4
+            ELSE 5
+        END AS id
+    FROM
+        venda)
+GROUP BY
+    id, price_range
+ORDER BY
+    id;
+
+-- Análise estatística da distribuição dos preços de aluguel na base de dados, agrupando as propriedades em diferentes
+-- faixas de preços e contabilizando os valores.
+SELECT
+    price_range,
+    COUNT(*) AS count_properties
+FROM (SELECT
+        CASE
+            WHEN price BETWEEN 0 AND 1000 THEN '0-1K'
+            WHEN price BETWEEN 1001 AND 2000 THEN '1K-2K'
+            WHEN price BETWEEN 2001 AND 3000 THEN '2K-3K'
+            WHEN price BETWEEN 3001 AND 4000 THEN '3K-4K'
+            ELSE '4K ou mais'
+        END AS price_range,
+        CASE
+            WHEN price BETWEEN 0 AND 1000 THEN 1
+            WHEN price BETWEEN 1001 AND 2000 THEN 2
+            WHEN price BETWEEN 2001 AND 3000 THEN 3
+            WHEN price BETWEEN 3001 AND 4000 THEN 4
+            ELSE 5
+        END AS id
+    FROM
+        aluguel)
+GROUP BY
+    id, price_range
+ORDER BY
+    id;
